5979e3642e3d83212d885983e6628c4e29885154
marwan-tabib <marwanmaroc297@gmail.com>
2022-10-19 12:56:24 +0200

    completed add task function

--- starter/assets/js/app.js
+++ starter/assets/js/app.js
@@ -3,14 +3,37 @@
  * 
  */
 
-function createTask() {
-    // initialiser task form
-
-    // Afficher le boutton save
-
-    // Ouvrir modal form
-    
+//---- Add Tasks ----//
+function add(){
+    let model = document.querySelector('.modal');
+        title = model.querySelector('#title').value,
+        type = model.querySelector('#type:checked').value,
+        priority = model.querySelector('#priority:checked').value,
+        status = model.querySelector('#status:checked').value,
+        date = model.querySelector('#date').value,
+        description = model.querySelector('#description').value;
+    var selector ;
+    status == "To Do" ? selector = "to-do-parent" : status == "In Progress" ? selector =  "in-progress-parent" : selector = "done-parent";
+    let add = 
+            {
+                'title'         :   title,
+                'type'          :   type,
+                'priority'      :   priority,
+                'status'        :   status,
+                'date'          :   date,
+                'description'   :   description
+            };
+    tasks.push(add);
+    show(tasks , selector , status); 
 }
+// function createTask() {
+//     // initialiser task form
+
+//     // Afficher le boutton save
+
+//     // Ouvrir modal form
+    
+// }
 
 function saveTask() {
     // Recuperer task attributes a partir les champs input
@@ -69,7 +92,7 @@ function initTaskForm() {
 
 function show(data , selector , status) {
     // Varaibles 
-        var parent = document.querySelector("#" + selector),
+        let parent = document.querySelector("#" + selector),
             child = parent.querySelector("#tasks"),
             title = child.querySelector('#title'),
             date = child.querySelector('#date'),
