f0344474625b9d3828520dace8f3983fa0d870f4
marwan-tabib <marwanmaroc297@gmail.com>
2022-10-28 16:51:30 +0200

    add delete button in assets

--- includes/layouts/task_container.php
+++ includes/layouts/task_container.php
@@ -31,14 +31,7 @@
 											</div>
 										</div>
 									</button>
-									<a href="javascript:void(0)"
-										onclick="if(confirm('Are You sure to delete this record?')){document.querySelector('#delete-task-<?php echo $row['id'] ?>').submit();} else {return false}"
-										class="btn btn-danger" id="delete-btn">
-											<i class="fa fa-trash"></i>
-									</a>
-									<form action="includes/scripts.php" method="post" class="d-none" id="delete-task-<?php echo $row['id'] ?>" >
-										<input type="hidden" name="delete" value="<?php echo $row['id'] ?>" >
-									</form>
+									<?php include'includes/layouts/delete-btn.php'; ?>
 								</a>
 							<?php } ?>
 						</div>
